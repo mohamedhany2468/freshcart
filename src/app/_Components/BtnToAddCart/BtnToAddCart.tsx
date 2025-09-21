1d4f243b0436553213b3d0e45eac408b14ed9905
mohamedhany2468 <mh4175982@gmail.com>
2025-09-21 18:28:22 +0300

    six commit

--- src/app/_Components/BtnToAddCart/BtnToAddCart.tsx
+++ src/app/_Components/BtnToAddCart/BtnToAddCart.tsx
@@ -4,7 +4,7 @@
 import { addToCart } from '@/CartActions/addtoCart'
 import { Button } from '@/components/ui/button'
 import { CartContext } from '@/context/CartContext'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { toast } from 'sonner'
 
 
@@ -26,8 +26,8 @@ interface AddProductResponse {
 
 function BtnToAddCart({ id, btn ,}: BtnToAddCartProps) {
    
-
-  // يجب تعريف نوع context بشكل صحيح في CartContext
+const [iLoading, setiLoading] = useState(false)
+  
   const cart = useContext(CartContext) as { AddProduct: (id: string) => Promise<AddProductResponse> };
   if (!cart) return null;
   const { AddProduct } = cart;
@@ -35,21 +35,24 @@ function BtnToAddCart({ id, btn ,}: BtnToAddCartProps) {
    
 
    async function hundelAddCart() {
+        setiLoading(true);
         const data = await AddProduct(id);
         console.log(data);
         if (data.status === "success") {
             toast.success(data.message ?? "Added successfully", {
                 position: "top-center"
             });
+setiLoading(false);
         } else {
             toast.error("faild to add this product in cart", {
                 position: "top-center"
             });
+            setiLoading(false);
         }
     }
   return (<>
-    {btn === true ? <Button  className=' border-1 border-[#B88E2F]   text-[#B88E2F] bg-white md:px-15 me-2 px-18 md:w-96 py-2 font-bold hover:bg-white  cursor-pointer  group-hover:block left' onClick={hundelAddCart}>Add to Crat</Button>:
-      <Button  className={`text-[#B88E2F] bg-white md:px-8 py-2 px-10 font-bold hover:bg-white  cursor-pointer  group-hover:block left`} onClick={hundelAddCart}>Add to Crat</Button>
+    {btn === true ? <Button  className=' border-1 border-[#B88E2F]   text-[#B88E2F] bg-white md:px-15 me-2 px-18 md:w-96 py-2 font-bold hover:bg-white  cursor-pointer  group-hover:block left' onClick={hundelAddCart}>{iLoading ?<i className="fa-solid fa-spinner fa-spin"></i> : "Add to Cart"}</Button>:
+      <Button  className={`text-[#B88E2F] bg-white md:px-8 py-2 px-10 font-bold hover:bg-white  cursor-pointer  group-hover:block left`} onClick={hundelAddCart}>{iLoading ?<i className="fa-solid fa-spinner fa-spin"></i> : "Add to Cart"}</Button>
 
     }
   </>
