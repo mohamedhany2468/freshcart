9bb6acb43603653b8cbf3d7743a6c1f30fccedab
mohamedhany2468 <mh4175982@gmail.com>
2025-09-27 18:01:53 +0300

    h  commit

--- src/app/_Components/CradHomePro/CradHomePro.tsx
+++ src/app/_Components/CradHomePro/CradHomePro.tsx
@@ -3,12 +3,17 @@ import { Button } from '@/components/ui/button'
 import { ProductType } from '@/types/Product.type'
 import Image from 'next/image'
 import React from 'react'
+import BtnToAddCart from '../BtnToAddCart/BtnToAddCart'
+import BtnAddWishlist from '../btnAddWishlist/btnAddWishlist'
+import Link from 'next/link'
 
 function CradHomePro({product}:{product:ProductType}) {
   return (
    
       
      <div className="card bg-base-100  shadow-sm w-[49%] group md:w-[31%] lg:w-[24%] mt-5 relative">
+       <Link href={`/productDetalies/${product.id}`}>
+       
   <figure>
     <Image
       src={product.imageCover}
@@ -20,8 +25,14 @@ function CradHomePro({product}:{product:ProductType}) {
     <p className='text-[20px] font-[600] md:text-md'>{product.price}EGP</p>
    
   </div>
+       </Link>
  <div className='absolute top-[100%] start-0 end-0 bottom-0 bg-neutral-950/70 flex justify-center items-center flex-wrap group-hover:top-0 duration-500 ease-in'>
-<Button  className=' hidden text-[#B88E2F] bg-white md:px-15 py-2 px-10 font-bold hover:bg-white  cursor-pointer  group-hover:block left'>Add to Crat</Button>
+  <div className=' sm:hidden flex justify-center items-center gap-2 mt-2'>
+
+<BtnToAddCart id={product.id} btn={true} /> 
+<BtnAddWishlist id={product.id} btn={false} /> 
+
+       </div>
  </div>
 </div>
 
